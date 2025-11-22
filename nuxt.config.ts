7c6c9d4226c62199e8e5fbf10575bb2bbbd3e1e1
Edson Isaac <edssaac@outlook.com>
2025-11-22 02:44:02 -0300

    feat: configura github actions

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,39 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
+
+  nitro: {
+    preset: 'github_pages'
+  },
+
   app: {
+    baseURL: '/rick-and-morty/',
     head: {
       title: 'Rick and Morty Data',
       link: [{
         rel: 'icon',
         type: 'image/svg+xml',
-        href: '/favicon.svg'
+        href: '/rick-and-morty/favicon.svg'
       }],
     },
   },
+
   css: ['assets/css/global.css'],
+
   image: {
     dir: 'assets/images'
   },
+
   compatibilityDate: '2025-07-15',
+
   devtools: { enabled: true },
+
   modules: [
     '@nuxt/image',
     '@nuxtjs/color-mode',
     '@nuxtjs/tailwindcss',
   ],
+
   runtimeConfig: {
     public: {
       apiBaseUrl: process.env.API_BASE_URL,
