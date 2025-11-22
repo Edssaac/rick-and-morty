a2c65e5f6968276105a001864d3af9f9bf1b2367
Edson Isaac <edssaac@outlook.com>
2025-11-22 13:02:21 -0300

    fix: bloqueia scroll do conteúdo ao abrir o modal

--- app/components/ui/modal/BaseModal.vue
+++ app/components/ui/modal/BaseModal.vue
@@ -7,10 +7,25 @@ const props = defineProps({
 })
 
 const emit = defineEmits(["update:modelValue"])
+const close = () => {
+    blockScroll(false)
+
+    emit("update:modelValue", false)
+}
 
 const open = computed(() => props.modelValue)
 
-const close = () => emit("update:modelValue", false)
+const blockScroll = (block: boolean = false) => {
+    if (block) {
+        document.documentElement.style.overflow = 'hidden'
+        document.body.style.overflow = 'hidden'
+    } else {
+        document.documentElement.style.overflow = ''
+        document.body.style.overflow = ''
+    }
+}
+
+blockScroll(true)
 
 </script>
 
@@ -19,7 +34,7 @@ const close = () => emit("update:modelValue", false)
         <transition name="fade">
             <div 
                 v-if="open" 
-                class="fixed inset-0 bg-black/60 flex items-center justify-center p-4 z-50"
+                class="fixed inset-0 bg-black/60 backdrop-blur-sm flex items-center justify-center p-4 z-50"
                 @click.self="close"
             >
                 <div class="bg-[#FFF] text-black dark:bg-[#1E1E1E] dark:text-white rounded-2xl shadow-xl p-6 max-h-[90vh] overflow-y-auto w-full max-w-2xl">
